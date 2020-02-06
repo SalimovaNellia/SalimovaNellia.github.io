d780778cc32c20d747e412c6f0fcd3cf500a773f
Nellia Salimova <salimova.nellia@gmail.com>
2020-02-06 14:22:06 +0200

    Animation for basket

---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+    createOptionList();
+
+    document.addEventListener("click", function(event) {
+        let target = event.target;
+        if (target.classList.contains("basket-btn") || target.classList.contains("basket-icon")) {
+            toggleBasket();
+        }
+
+        if (target.classList.contains("open")) {
+            toggleBasket();
+        }
+    });
+
+    let closeBasket = document.getElementById("closeBasket");
+    closeBasket.addEventListener("click", toggleBasket)
+
+};
+
+function toggleBasket() {
+    let basketPopup = document.getElementById("basketPopup");
+    basketPopup.classList.toggle("open");
+}
+
+function openList() {
+    let optionsList = document.getElementById("optionsListWrap");
+    optionsList.classList.toggle("open");
+}
+
+
+function createOptionList() {
+    let optionsList = document.getElementById("optionsList");
+
+    for (let i = 0; i <= 49; i++) {
+        let li = document.createElement("li");
+        li.className = "option";
+        li.id = "option-" + i;
+        let span = document.createElement("span");
+        span.innerHTML = i + 1;
+        li.appendChild(span);
+        optionsList.appendChild(li);
+    }
+
+    let selectIcon = document.getElementById("selectIcon");
+    selectIcon.onclick = openList;
+}
+
